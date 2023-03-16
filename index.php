bb70d1a1289c02fe1baa5adc8c6d9f40b865cfc7
Rundredoffi <78939327+Rundredoffi@users.noreply.github.com>
2023-03-16 16:55:11 +0100

    Changement du type de site internet
    Modification complète de la structure du site internet
    > Passage sur PHP
    > Système templating 
    >>Footer
    >>Header
    > Restructuration des fichiers (Nouveau dossier pour le stockage de ceci

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require("/template/header.html");
+date_default_timezone_set('Europe/Paris')
+?>
+
+<title>Harbor Cleaner</title>
+Ici placez tout le code HTML que vous désirez.
+(...)
+
+
+<?php
+require("/template/footer.html");
+?>
