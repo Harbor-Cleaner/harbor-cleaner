6e46db9d3f1bf13aa46d43959f559fb18ea8c808
Rundredoffi <contact.rundred@gmail.com>
2023-04-03 14:28:40 +0200

    Header

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 require("template/header.html");
 date_default_timezone_set('Europe/Paris');
 ?>
-
-<title>Harbor Cleaner</title>
 <!-- Présentation du projet -->
-<h1>Harbor Cleaner</h1>
 <a id="projet"></a>
 <!-- Présentation but du projet -->
 <h2>Le but du projet ?</h2>
@@ -30,17 +27,43 @@ While ($donnees = $reponse->fetch()) // Rend les valeurs utilisables et les affi
 
     echo'
     <div class="display-nombres">
+    <table>
+        <thead>
+            <tr>
+                <th colspan="2">Notre projet en quelques chiffres</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Distance parcourue</th>
+                <th>Déchet récolté</th>
+                <th>Heure de fonctionnement</th>
+                <th>Etat</th>
+            </tr>
+            <tr>
+                <th>Distance parcourue</th>
+                <th>Déchet récolté</th>
+                <th>Heure de fonctionnement</th>
+                <th>Etat</th>
+            </tr>
+                <th>'.$donnees['distance'].'</th>
+            </tr>
+        </tbody>
+    </table>
         <ul>
-            <li> Distance parcourues : '.$donnees['distance'].'kms </li>
-            <li> Déchet récoltés :'.$donnees['dechet'].'t </li>
-            <li> Heure de fonctionnement : '.$donnees['heure'].'h </li>
-            <li> Etat :'.$donnees['etat'].'</li>
+            <li> Distance parcourues : <br>kms </li>
+            <li> Déchet récoltés : <br>'.$donnees['dechet'].'t </li>
+            <li> Heure de fonctionnement : <br>'.$donnees['heure'].'h </li>
+            <li> Etat : <br>'.$donnees['etat'].'</li>
         </ul>
     </div>
     ';
     }
 $reponse->closeCursor();  // Termine le traitement de la requête
 ?>
+
+
+
 <?php
 require("template/footer.html");
 ?>
