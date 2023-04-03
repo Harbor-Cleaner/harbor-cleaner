2b0fbc937774c5b4080452cc1616f1c2445e7eac
Nicolas Jouin--Derrien <contact.rundred@gmail.com>
2023-04-03 20:01:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ catch(Exception $e)
     die('Erreur : '.$e->getMessage());
 }
 // Affichage de données
-$reponse = $bdd->query('SELECT * FROM `bateau` ORDER BY distance DESC LIMIT 1'); //Requête BDD
+$reponse = $bdd->query('SELECT * FROM `bateau` ORDER BY dechet DESC LIMIT 1'); //Requête BDD
 While ($donnees = $reponse->fetch()) // Rend les valeurs utilisables et les affiche
 {
     echo'
