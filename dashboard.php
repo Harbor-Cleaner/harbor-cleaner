ab148118df8edfa290b3ae0e2d7b2998814f5857
Rundredoffi <contact.rundred@gmail.com>
2023-04-13 14:57:35 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -8,16 +8,16 @@ date_default_timezone_set('Europe/Paris')
 <div class="split-screen">
     <!-- Contenue gauche -->
     <div class="split-screen__half">
-        <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21358.88595380644!2d-4.10964305!3d47.9970783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4810d5925b5c02cf%3A0x19d6d491f66ae9e0!2sCath%C3%A9drale%20Saint-Corentin!5e0!3m2!1sfr!2sfr!4v1678975896937!5m2!1sfr!2sfr"
-            width="500" 
-            height="500" 
-            style="border:0;" 
-            allowfullscreen="" 
-            loading="lazy" 
-            referrerpolicy="no-referrer-when-downgrade"
-            id="carte">
-        </iframe>
+    <iframe
+    id="carte"
+    width="425" 
+    height="350" 
+    frameborder="0" 
+    scrolling="no" 
+    marginheight="0" 
+    marginwidth="0" 
+    src="https://www.openstreetmap.org/export/embed.html?bbox=-4.942474365234375%2C47.622826666563675%2C-3.1297302246093754%2C48.43284538647477&amp;layer=mapnik">
+    </iframe>
     </div>
     <!-- Contenue droite -->
     <div class="split-screen__half">
