355076ba1abfaba65221ee68581bf55cd31de81b
Rundredoffi <contact.rundred@gmail.com>
2023-05-22 11:01:42 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta http-equiv="refresh" content="5"><!-- actualisation toutes les 30 secondes -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
