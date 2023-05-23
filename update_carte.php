473619a13720861afbd0b9ea68c8e484297b85ec
Rundredoffi <contact.rundred@gmail.com>
2023-05-23 13:57:28 +0200

    Update update_carte.php

--- update_carte.php
+++ update_carte.php
@@ -13,8 +13,8 @@ if ($conn->connect_error) {
     die("Échec de la connexion à la base de données : " . $conn->connect_error);
 }
 
-// Récupérer les coordonnées lat et lon depuis la base de données
-$sql = "SELECT lat, lon FROM bateau";
+// Récupérer les coordonnées lat et lon du dernier bateau ajouté
+$sql = "SELECT lat, lon FROM bateau ORDER BY bateau_id DESC LIMIT 1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -25,7 +25,7 @@ if ($result->num_rows > 0) {
     // Construire l'URL avec les nouvelles coordonnées
     $wazeUrl = "https://embed.waze.com/iframe?zoom=16&lat=" . $lat . "&lon=" . $lon . "&pin=1";
 
-    // Renvoyer l'URL mis à jour
+    // Renvoyer l'URL mise à jour
     echo $wazeUrl;
 } else {
     echo ""; // Gérer le cas où il n'y a pas de données dans la base de données
